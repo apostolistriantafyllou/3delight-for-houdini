0e2bac918c277363b2ea98a2805fd68727a210e2
aghiles <aghiles@3delight.com>
2019-07-14 14:05:02 -0400

    Implement proper camera field of view.
    Renders are now correct (same as Mantra and OpenGL view).

--- camera.cpp
+++ camera.cpp
@@ -26,6 +26,16 @@ void camera::set_attributes( void ) const
 {
 }
 
+/**
+	\brief Sets the field of view, clipping planes and depth of field parameters
+	of this camera.
+
+	NOTE: NSI expects a horizontal field of view and the description we get from
+	Houdini is for a vertical FOV.
+
+	TODO:
+	depth of field.
+*/
 void camera::set_attributes_at_time( double i_time ) const
 {
 	OBJ_Camera* cam = m_object->castToOBJCamera();
@@ -39,6 +49,12 @@ void camera::set_attributes_at_time( double i_time ) const
 	double nsi_shutter[2] = { i_time - shutter/2.0, i_time + shutter/2.0 };
 
 	double nsi_clip[2] = { cam->getNEAR(i_time), cam->getFAR(i_time) };
+
+	/* compute horizontal field of view */
+	float V = 2.0f * atan2f( cam->APERTURE(i_time) * 0.5, cam->FOCAL(i_time));
+	double ratio = cam->RESX(i_time) / double(cam->RESY(i_time));
+	float horizontal_fov = 2.0 * ::atan( ::tanf(V*0.5f) * 1/ratio );
+	horizontal_fov *=  360.f / (2.0 * M_PI); // to degrees
 
 	m_nsi.SetAttribute(
 		m_handle,
@@ -58,7 +74,7 @@ void camera::set_attributes_at_time( double i_time ) const
 		m_nsi.SetAttribute(
 			m_handle,
 			(
-				NSI::FloatArg("fov",  cam->APERTURE(i_time)),
+				NSI::FloatArg("fov",  horizontal_fov),
 				// FIXME : DOF is broken
 				NSI::IntegerArg("depthoffield.enable", false),//m_context.m_dof),
 				NSI::DoubleArg("depthoffield.fstop", cam->FSTOP(i_time)),
