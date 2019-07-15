apostolistriantafyllou/3delight-for-houdini

commit e576589154d72b756ee1d1b18c09719f82960cc6

Put code for OSL name extraction to shader_library
Its place is there. The vop can now just call the shader_library
with whatever the VOP_Node operator's name is. Also added some
needed comments.

shader_library.cpp
@@ -58,26 +58,57 @@ DlShaderInfo *shader_library::get_shader_info( const char *path ) const
 
 /**
 	\param name
-		The name of the shader to look for without extension
+		The name of the operator to look for without extension
 
 	Just a quick implementation to get us to the next step:
 	- we check plug-ins installation path + OSO
 	- and some developper path (to be removed later on).
 */
-std::string shader_library::get_shader_path( const char *name ) const
+std::string shader_library::get_shader_path( const char *vop ) const
 {
+	std::string name = vop_to_osl(vop);
+	if( name.size() == 0 )
+		return {};
+
 	std::string installation_path =
 		m_plugin_path + "/../osl/" + name + ".oso";
 	if( file_exists( installation_path.c_str()) )
 		return installation_path;
 
 // FIXME : wrong, hardcoded path
-	std::string dl_shaders_path = 
+	std::string dl_shaders_path =
 		m_plugin_path + "/../external-osl/" + name + ".oso";
 	if( file_exists( dl_shaders_path.c_str()) )
 		return dl_shaders_path;
 
 	return {};
+}
+
+std::string shader_library::vop_to_osl( const char *i_vop )
+{
+	std::string legalized( i_vop );
+
+	auto p1 = legalized.find( "::" );
+	if( p1 == std::string::npos )
+	{
+		/* no occurences, nothing to do. */
+		return legalized;
+	}
+
+	auto p2 = legalized.find( "::", p1+2 );
+
+	/* Legalize for file system */
+	std::replace( legalized.begin(), legalized.end(), '.', '_' );
+	std::replace( legalized.begin(), legalized.end(), ':', '_' );
+
+	if( p2 == std::string::npos )
+	{
+		/* only one occurence, no name space */
+		return legalized;
+	}
+
+	/* two occurences, skip namespace */
+	return legalized.substr(p2 + 2);
 }
 
 static void

ROP_3Delight.cpp
@@ -4,7 +4,6 @@
 #include "scene.h"
 #include "context.h"
 #include "shader_library.h"
-#include "vop.h"
 
 #include <OBJ/OBJ_Camera.h>
 #include <OP/OP_Director.h>
@@ -539,9 +538,7 @@ void ROP_3Delight::ExportDefaultMaterial( const context &i_context ) const
 
 	NSI::Context &nsi = i_context.m_nsi;
 	const shader_library &library = shader_library::get_instance();
-	std::string path =
-		library.get_shader_path(
-			vop::osl_name("principledshader::2.0").c_str() );
+	std::string path = library.get_shader_path( "principledshader::2.0" );
 
 	nsi.Create( k_shader, "shader" );
 	nsi.Create( k_attributes, "attributes" );

shader_library.h
@@ -20,10 +20,23 @@ public:
 	shader_library();
 
 public:
-	std::string get_shader_path( const char *i_name ) const;
+	std::string get_shader_path( const char *i_vop_name ) const;
 	DlShaderInfo *get_shader_info( const char *i_path ) const;
 
-	void Register(OP_OperatorTable* io_table)const;
+	void Register( OP_OperatorTable* io_table) const;
+
+	/**
+		\brief given an operator name, returns the OSL name.
+
+		\param i_vop
+			The name of the operator as returned by
+			VOP_Node::getOperator()->getName().
+
+		\returns a name usable on the file system.
+
+		https://www.sidefx.com/docs/houdini/assets/namespaces.html
+	*/
+	static std::string vop_to_osl( const char *i_vop );
 
 public:
 	std::string m_plugin_path;

vop.cpp
@@ -23,7 +23,7 @@ vop::vop(
 void vop::create( void ) const
 {
 	const shader_library &library = shader_library::get_instance();
-	std::string path = library.get_shader_path( osl_name().c_str() );
+	std::string path = library.get_shader_path( vop_name().c_str() );
 	if( path.length() == 0 )
 		return;
 
@@ -115,7 +115,7 @@ void vop::list_shader_parameters(
 	NSI::ArgumentList &o_list )
 {
 	const shader_library &library = shader_library::get_instance();
-	std::string path = library.get_shader_path( osl_name(i_shader).c_str() );
+	std::string path = library.get_shader_path( i_shader );
 	if( path.size() == 0 )
 	{
 		return;
@@ -190,25 +190,16 @@ void vop::list_shader_parameters(
 	}
 }
 
-std::string vop::osl_name( void ) const
+std::string vop::vop_name( void ) const
 {
-	return osl_name( m_vop->getOperator()->getName().buffer() );
+	return m_vop->getOperator()->getName().buffer();
 }
 
-std::string vop::osl_name( const char *i_shader )
-{
-	std::string legalized( i_shader );
-
-	std::replace( legalized.begin(), legalized.end(), ':', '_' );
-	std::replace( legalized.begin(), legalized.end(), '.', '_' );
-
-	return legalized;
-}
 
 bool vop::ignore_subnetworks( void ) const
 {
 	const shader_library &library = shader_library::get_instance();
-	std::string path = library.get_shader_path( osl_name().c_str() );
+	std::string path = library.get_shader_path( vop_name().c_str() );
 	if( path.size() == 0 )
 	{
 		return false;
@@ -235,6 +226,6 @@ bool vop::ignore_subnetworks( void ) const
 bool vop::unsupported( void ) const
 {
 	const shader_library &library = shader_library::get_instance();
-	std::string path = library.get_shader_path( osl_name().c_str() );
+	std::string path = library.get_shader_path( vop_name().c_str() );
 	return path.size() == 0;
 }

vop.h
@@ -28,14 +28,9 @@ public:
 	void connect( void ) const override;
 
 	/**
-		\returns a legalized OSL name from vop->getOperator()->getName()
-		as these names can contain illegal characters for a file system
-		and/or OSL.
-
-		principled::2.0 would be converted to principled__2_0.
+		\returns m_vop->getOperator()->getName()
 	*/
-	std::string osl_name( void ) const;
-	static std::string osl_name( const char * );
+	std::string vop_name( void ) const;
 
 protected:
 	/**

commit aa187c95a40c0d692394cd8c0a5847b961786410

Make the 3Delight VOP namespace work.
There was a small bug in shader_library::vop_to_osl (introduced in
e576589154d72). So starting from now, we can use the shader created
by VOP_ExternalOSL, which means we can use our Material library :)

shader_library.cpp
@@ -81,9 +81,13 @@ std::string shader_library::get_shader_path( const char *vop ) const
 	if( file_exists( dl_shaders_path.c_str()) )
 		return dl_shaders_path;
 
+	std::cout << "cannot find : " << dl_shaders_path << std::endl;
 	return {};
 }
 
+/**
+	FIXME: use regex match.
+*/
 std::string shader_library::vop_to_osl( const char *i_vop )
 {
 	std::string legalized( i_vop );
@@ -103,8 +107,9 @@ std::string shader_library::vop_to_osl( const char *i_vop )
 
 	if( p2 == std::string::npos )
 	{
-		/* only one occurence, no name space */
-		return legalized;
+		/* only one occurence, could be namespace or version */
+		return std::isdigit(legalized[p1+2]) ?
+			legalized : legalized.substr(p1+2);
 	}
 
 	/* two occurences, skip namespace */