221129a157b8853ca0bc7af04428cf0f2d56bee5
aghiles <aghiles@3delight.com>
2019-07-15 15:28:29 -0400

    Add support for points.
    This is the most "raw" support until now: output all attributes to
    the NSI context. "P" and "width" variables have the name as in NSI.
    Note that Mantra seems to render the particles larger.

--- pointmesh.cpp
+++ pointmesh.cpp
@@ -22,12 +22,43 @@ void pointmesh::create( void ) const
 
 void pointmesh::set_attributes( void ) const
 {
-     const GT_PrimPointMesh *pointmesh =
-	    static_cast<const GT_PrimPointMesh *>(m_gt_primitive.get());
-
-	 pointmesh->dumpAttributeLists( m_handle.c_str(), true );
 }
 
-void pointmesh::set_attributes_at_time( double ) const
+void pointmesh::set_attributes_at_time( double i_time ) const
 {
+	const GT_PrimPointMesh *pointmesh =
+		static_cast<const GT_PrimPointMesh *>(m_gt_primitive.get());
+
+	const GT_AttributeListHandle &attributes = pointmesh->getPointAttributes() ;
+	const UT_StringArray &names = attributes->getNames();
+
+	for( int i = 0; i<names.entries(); i++ )
+	{
+		const GT_DataArrayHandle &data = attributes->get( names[i] );
+		GT_Type type = data->getTypeInfo();
+		NSIType_t nsi_type = NSITypeInvalid;
+
+		switch( type )
+		{
+			case GT_TYPE_POINT: nsi_type = NSITypePoint; break;
+			case GT_TYPE_COLOR: nsi_type = NSITypeColor; break;
+			case GT_TYPE_VECTOR: nsi_type = NSITypeVector; break;
+			case GT_TYPE_NORMAL: nsi_type = NSITypeNormal; break;
+			case GT_TYPE_NONE: nsi_type = NSITypeFloat; break;
+			default:
+				std::cout << "unsupported attribute type " << type << " of name "
+					<< names[i] << " on " << m_handle;
+			continue;
+		}
+
+		GT_DataArrayHandle buffer_in_case_we_need_it;
+
+		m_nsi.SetAttributeAtTime( m_handle, i_time,
+			*NSI::Argument( names[i].buffer() )
+				.SetType( nsi_type )
+				->SetCount( data->entries() )
+				->SetValuePointer( data->getF32Array(buffer_in_case_we_need_it)));
+	}
+
+
 }
