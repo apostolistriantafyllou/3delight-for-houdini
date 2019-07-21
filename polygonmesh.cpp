apostolistriantafyllou/3delight-for-houdini

commit f51021f3bea67e8b5f266355702408bed73ecd8d

Add texture coordinate export to polys/subdivs.
This uses the newly introduced exporter::export_attributes which is
becoming very handy. We export UV coordinate only in set_attributes()
so that motion blur (when implemented) won't have thease repeated
for each segment.

polygonmesh.cpp
@@ -77,40 +77,44 @@ void polygonmesh::set_attributes( void ) const
 	}
 
 	m_nsi.SetAttribute( m_handle.c_str(), mesh_args );
+
+	/* output the UVs only once */
+	GT_AttributeListHandle attributes[] =
+	{
+		polygon_mesh->getShared(),
+		polygon_mesh->getVertex(),
+		polygon_mesh->getUniform()
+	};
+	std::vector<const char *> to_export{ "uv" };
+	exporter::export_attributes(
+		attributes,
+		sizeof(attributes)/sizeof(attributes[0]),
+		m_context.m_start_time,
+		to_export );
 }
 
 /**
-	\brief output P, N and all the attributes for this polygon mesh.
+	\brief output P, N
 */
-void polygonmesh::set_attributes_at_time( double ) const
+void polygonmesh::set_attributes_at_time( double i_time ) const
 {
-     const GT_PrimPolygonMesh *polygon_mesh =
-	    static_cast<const GT_PrimPolygonMesh *>(m_gt_primitive.get());
+	const GT_PrimPolygonMesh *polygon_mesh =
+		static_cast<const GT_PrimPolygonMesh *>(m_gt_primitive.get());
 
-	 const GT_DataArrayHandle &P = polygon_mesh->getShared()->get( GA_Names::P );
-
-	 NSI::ArgumentList mesh_args;
-
-	 GT_DataArrayHandle buffer_in_case_we_need_it;
-	 mesh_args.Add( NSI::Argument::New( "P" )
-		 ->SetType( NSITypePoint )
-		 ->SetCount( P->entries() )
-		 ->SetValuePointer( P->getF32Array(buffer_in_case_we_need_it) ) );
-
-	std::unique_ptr<UT_Vector3> N;
-	static_assert( sizeof(UT_Vector3) == sizeof(float)*3,
-		"UT_Vector3 is not of the expected size/type" );
-
-	if( !m_is_subdiv )
+	GT_AttributeListHandle attributes[] =
 	{
-		N = std::unique_ptr<UT_Vector3>(new UT_Vector3[P->entries()]);
-		polygon_mesh->pointNormals( N.get(), P->entries() );
+		polygon_mesh->getShared(),
+		polygon_mesh->getVertex(),
+		polygon_mesh->getUniform()
+	};
 
-		mesh_args.Add( NSI::Argument::New( "N" )
-			->SetType( NSITypeNormal )
-			->SetCount( P->entries() )
-			->SetValuePointer(N.get()) );
-	}
+	std::vector<const char *> to_export{ "P" };
+	if( !m_is_subdiv )
+		to_export.push_back( "N" );
 
-	m_nsi.SetAttribute( m_handle.c_str(), mesh_args );
+	exporter::export_attributes(
+		attributes,
+		sizeof(attributes)/sizeof(attributes[0]),
+		i_time,
+		to_export );
 }

exporter.cpp
@@ -124,6 +124,7 @@ NSIType_t gt_to_nsi_type( GT_Type i_type )
 	case GT_TYPE_POINT: return  NSITypePoint;
 	case GT_TYPE_COLOR: return  NSITypeColor;
 	case GT_TYPE_VECTOR: return  NSITypeVector;
+	case GT_TYPE_TEXTURE: return  NSITypeFloat; // it's a float float[3]
 	case GT_TYPE_NORMAL: return  NSITypeNormal;
 	case GT_TYPE_NONE: return  NSITypeFloat;
 	default:
@@ -132,6 +133,13 @@ NSIType_t gt_to_nsi_type( GT_Type i_type )
 	return NSITypeInvalid;
 }
 
+/**
+	gotchas:
+	- When we find a texture coordinate attribute, we output "st".
+	  regardless of the name we find (usually "uv"). This is to be
+	  consistant with other packages.  After the first one we add a
+	  number.
+*/
 void exporter::export_attributes(
 	const GT_AttributeListHandle *i_attributes,
 	int i_n, double i_time,
@@ -162,8 +170,19 @@ void exporter::export_attributes(
 			}
 
 			it = io_which_ones.erase( it );
-
 			GT_DataArrayHandle buffer_in_case_we_need_it;
+
+			if( data->getTypeInfo() == GT_TYPE_TEXTURE )
+			{
+				m_nsi.SetAttributeAtTime( m_handle, i_time,
+					*NSI::Argument("st")
+						.SetArrayType( NSITypeFloat, 3)
+						->SetCount( data->entries() )
+						->SetValuePointer(
+							data->getF32Array(buffer_in_case_we_need_it)));
+				continue;
+			}
+
 			m_nsi.SetAttributeAtTime( m_handle, i_time,
 				*NSI::Argument(name)
 					.SetType( nsi_type )

osl/texture__2_0.osl
@@ -16,8 +16,8 @@ shader texture__2_0 [[ string tags[1] = {"ignoresubnetworks"} ]] (
 	int interp = 0,
 	output color clr = 0 )
 {
-	float st[2] = {u, v};
+	float st[3] = {u, v, 0};
 	getattribute( "st", st );
 	clr = texture(
-		map, st[0], st[1], "width", width, "blur", blur, "wrap", "periodic");
+		map, st[0], 1-st[1], "width", width, "blur", blur, "wrap", "periodic");
 }

commit e6cb777a027c0cf15cb1e2c47a2629e9ee7fafcf

Fix normals export in polygon meshes.
It was broken by f51021f3bea67e8b5f266355702408bed73ecd8d as Houdini
polygon meshes do not seem to contain the "N" variable by default so
you have to force its computation.

polygonmesh.cpp
@@ -95,11 +95,22 @@ void polygonmesh::set_attributes( void ) const
 
 /**
 	\brief output P, N
+
+	\ref http://www.sidefx.com/docs/hdk/_g_t___prim_polygon_mesh_8h_source.html
 */
 void polygonmesh::set_attributes_at_time( double i_time ) const
 {
 	const GT_PrimPolygonMesh *polygon_mesh =
 		static_cast<const GT_PrimPolygonMesh *>(m_gt_primitive.get());
+
+	const GT_PrimPolygonMesh *with_normals =  nullptr;
+	if( !m_is_subdiv )
+	{
+		with_normals = polygon_mesh->createPointNormalsIfMissing();
+	}
+
+	if( with_normals != nullptr )
+		polygon_mesh = with_normals;
 
 	GT_AttributeListHandle attributes[] =
 	{
@@ -117,4 +128,6 @@ void polygonmesh::set_attributes_at_time( double i_time ) const
 		sizeof(attributes)/sizeof(attributes[0]),
 		i_time,
 		to_export );
+
+	delete with_normals;
 }