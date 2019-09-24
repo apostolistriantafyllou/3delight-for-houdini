77bbe5ef3e082ccc9298e832dba3086f9bb2f463
Francois Colbert <fc@3delight.com>
2019-09-24 18:32:14 -0400

    Ensured the plugin's entry points are exported properly on Windows.

--- plugin.cpp
+++ plugin.cpp
@@ -4,13 +4,13 @@
 #include <UT/UT_DSOVersion.h>
 
 
-extern "C" void
+extern "C" SYS_VISIBILITY_EXPORT void
 newDriverOperator(OP_OperatorTable* io_table)
 {
 	ROP_3Delight::Register(io_table);
 }
 
-extern "C" void
+extern "C" SYS_VISIBILITY_EXPORT void
 newVopOperator(OP_OperatorTable* io_table)
 {
 	shader_library::get_instance().Register(io_table);
