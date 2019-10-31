5139228cb71228abf2524c6c574c70c3897b7f9d
Yves Claude <yves@3delight.com>
2019-10-30 20:37:54 -0400

    Add prefix to avoid clash with other plugins.

--- scripts/obj/geo.cmd
+++ scripts/obj/geo.cmd
@@ -6,18 +6,18 @@ source $HH/scripts/obj/geo.cmd
 \set noalias = 1
 if ( "$arg1" != "" ) then
 #   message Some new informative message for $arg1
-   opproperty -f $arg1 3Delight obj_geo_label1_group
-   opproperty -f $arg1 3Delight vis_diffuse
-   opproperty -f $arg1 3Delight compositing
-   opproperty -f $arg1 3Delight obj_geo_label2_group
-   opproperty -f $arg1 3Delight deformation
-   opproperty -f $arg1 3Delight add_samples
-   opproperty -f $arg1 3Delight obj_geo_label3_group
-   opproperty -f $arg1 3Delight override_vol
-   opproperty -f $arg1 3Delight over_compositing_group
-   opproperty -f $arg1 3Delight over_vis_camera_group
-   opproperty -f $arg1 3Delight over_vis_diffuse_group
-   opproperty -f $arg1 3Delight over_vis_reflection_group
-   opproperty -f $arg1 3Delight over_vis_refraction_group
-   opproperty -f $arg1 3Delight override_ss
+   opproperty -f $arg1 3Delight _3dl_obj_geo_label1_group
+   opproperty -f $arg1 3Delight _3dl_vis_diffuse
+   opproperty -f $arg1 3Delight _3dl_compositing
+   opproperty -f $arg1 3Delight _3dl_obj_geo_label2_group
+   opproperty -f $arg1 3Delight _3dl_deformation
+   opproperty -f $arg1 3Delight _3dl_add_samples
+   opproperty -f $arg1 3Delight _3dl_obj_geo_label3_group
+   opproperty -f $arg1 3Delight _3dl_override_vol
+   opproperty -f $arg1 3Delight _3dl_over_compositing_group
+   opproperty -f $arg1 3Delight _3dl_over_vis_camera_group
+   opproperty -f $arg1 3Delight _3dl_over_vis_diffuse_group
+   opproperty -f $arg1 3Delight _3dl_over_vis_reflection_group
+   opproperty -f $arg1 3Delight _3dl_over_vis_refraction_group
+   opproperty -f $arg1 3Delight _3dl_override_ss
 endif
