7759b7af4db5285ee35438b351642032a3ff5e91
Francois Colbert <fc@3delight.com>
2019-11-04 16:18:18 -0500

    Used the object's motion blur parameters to determine the number of time samples.

--- time_sampler.cpp
+++ time_sampler.cpp
@@ -5,6 +5,9 @@
 
 namespace
 {
+	const char* k_add_deformation_samples = "_3dl_deformation";
+	const char* k_nb_additional_samples = "_3dl_add_samples";
+
 	/*
 		Calls OP_Node::isTimeDependent on i_node without using a temporary
 		OP_Context, which we can't because isTimeDependent's argument is
@@ -27,4 +30,27 @@ time_sampler::time_sampler(const context& i_context, OBJ_Node& i_node)
 			: 0),
 		m_current_sample(0)
 {
+	/*
+		Here, we presume that the presence of k_nb_additional_samples implies
+		that our other motion blur parameters are present, too.
+	*/
+	if(i_context.MotionBlur() && i_node.hasParm(k_nb_additional_samples))
+	{
+		UT_String mb;
+		i_node.evalString(
+			mb, k_add_deformation_samples, 0, i_context.m_current_time);
+
+		// This avoids testing i_node.isTimeDependent a second time
+		bool time_dependent = m_nb_intervals > 0;
+
+		if(mb == "off")
+		{
+			m_nb_intervals = 0;
+		}
+		else if(time_dependent || mb == "on2")
+		{
+			m_nb_intervals = 1 +
+				i_node.evalInt(k_nb_additional_samples, 0, i_context.m_current_time);
+		}
+	}
 }
