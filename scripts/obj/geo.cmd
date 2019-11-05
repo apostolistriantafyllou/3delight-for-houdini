58825ffe7d1103f0f07c1784f1cc2fd14b400372
Yves Claude <yves@3delight.com>
2019-11-04 19:23:56 -0500

    Removed old properties (without prefix).

--- scripts/obj/geo.cmd
+++ scripts/obj/geo.cmd
@@ -5,7 +5,35 @@ source $HH/scripts/obj/geo.cmd
 
 \set noalias = 1
 if ( "$arg1" != "" ) then
-#   message Some new informative message for $arg1
+# Removed old names (without prefix)
+   opspare -d obj_geo_separator1 $arg1
+   opspare -d obj_geo_label1 $arg1
+   opspare -d obj_geo_separator2 $arg1
+   opspare -d space2 $arg1
+   opspare -d vis_diffuse $arg1
+   opspare -d compositing $arg1
+   opspare -d obj_geo_separator3 $arg1
+   opspare -d obj_geo_label2 $arg1
+   opspare -d obj_geo_separator4 $arg1
+   opspare -d space3 $arg1
+   opspare -d deformation $arg1
+   opspare -d add_samples $arg1
+   opspare -d obj_geo_separator5 $arg1
+   opspare -d obj_geo_label3 $arg1
+   opspare -d obj_geo_separator6 $arg1
+   opspare -d override_vol $arg1
+   opspare -d over_compositing_enable $arg1
+   opspare -d over_compositing $arg1
+   opspare -d over_vis_camera_enable $arg1
+   opspare -d over_vis_camera $arg1
+   opspare -d over_vis_diffuse_enable $arg1
+   opspare -d over_vis_diffuse $arg1
+   opspare -d over_vis_reflection_enable $arg1
+   opspare -d over_vis_reflection $arg1
+   opspare -d over_vis_refraction_enable $arg1
+   opspare -d over_vis_refraction $arg1
+   opspare -d override_ss $arg1
+# Add properties into tab 3Delight
    opproperty -f $arg1 3Delight _3dl_obj_geo_label1_group
    opproperty -f $arg1 3Delight _3dl_vis_diffuse
    opproperty -f $arg1 3Delight _3dl_compositing
