bdeec6eebdfd5c54d7c5f099f2af5f3810210d1d
Francois Colbert <fc@3delight.com>
2019-12-18 17:45:39 -0500

    Passed Python commands into a string argument instead of using a separate file.
    $arg0 gets an absolute path when the script is called from Houdini, but a
    relative one when it's called from our 456.cmd, so evaluating $arg0.py doesn't
    work in that case.
    
    Although it's a bit harder to read, this way of executing the Python code also
    has the advantage of not needing an extra file and keeping the patch close to
    the problem it fixes.

--- scripts/obj/hlight-2.0_OnCreated.cmd
+++ scripts/obj/hlight-2.0_OnCreated.cmd
@@ -1,13 +1,33 @@
-# Default script run when a node has been created
+# Script run when a light node has been created
 
-# message obj/hlight-2.0_OnCreated is called with $arg1
+# message $arg0 is called with $arg1
 \set noalias = 1
 if ( "$arg1" != "" ) then
-    opproperty -f $arg1 3Delight _3dl_spread
-# patch to move folder after the tab "Misc"
-	python $arg0.py $arg1
-    opproperty -f $arg1 3Delight _3dl_obj_light_contrib_title
-    opproperty -f $arg1 3Delight _3dl_contributions_group
-    opproperty -f $arg1 3Delight _3dl_compositing_group_title
-    opproperty -f $arg1 3Delight _3dl_light_prelit
+
+	opproperty -f $arg1 3Delight _3dl_spread
+	opproperty -f $arg1 3Delight _3dl_obj_light_contrib_title
+	opproperty -f $arg1 3Delight _3dl_contributions_group
+	opproperty -f $arg1 3Delight _3dl_compositing_group_title
+	opproperty -f $arg1 3Delight _3dl_light_prelit
+
+	# The 3Delight tab is erroneously added inside an existing tab instead of at
+	# the root.  Let's fix this by removing the 3Delight folder from the node
+	# and re-inserting it after the "Misc" folder, which is always present.
+	# This can't seem to be done in HScript, so we do it in Python.
+	# However, since calling an external Python script doesn't always work, we
+	# pass the Python statements directly in a string argument.
+	# The strange quoting around $arg1, below, is the result of quoting a first
+	# string with single-quotes in order for the double-quote to appear in it,
+	# then juxtaposing (without spaces, to enable concatenation) a double-quoted
+	# string that allows the substitution of arg1's value, then juxtaposing
+	# (again without spaces), another single-quoted string that starts with the
+	# closing double-quote that matches the one in the first string.
+
+	python -c 'node = hou.node("'"$arg1"'"); \
+		group = node.parmTemplateGroup(); \
+		dl_folder = group.containingFolder("_3dl_spread"); \
+		group.remove(dl_folder); \
+		group.insertAfter(group.findIndicesForFolder("Misc"), dl_folder); \
+		node.setParmTemplateGroup(group)'
+
 endif
