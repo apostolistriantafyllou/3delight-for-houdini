b982f6a257c352724641237fde25372b2d433830
Yves Claude <yves@3delight.com>
2020-02-19 14:01:21 -0500

    Don't redo connexions when ogl parameters already exist.
    Also, fix broken test from Chaos Man (see commit
    086fa1a5a114b142f17a7aa37d286c1868118f40).

--- ui/3Delight--VOP_OnCreated.cmd
+++ ui/3Delight--VOP_OnCreated.cmd
@@ -10,14 +10,14 @@ if ( "$arg1" != "" ) then
 		set ogl_diff_def = `run("opparm -ql $arg1 ogl_diff")`
 		if("$ogl_diff_def" == "") then
 			opproperty -f -F OpenGL $arg1 material* ogl_diff
-		endif
 
-		chadd -t 0 0 $arg1 ogl_diffr
-		chkey -t 0 -F 'ch("i_colorr")' $arg1/ogl_diffr
-		chadd -t 0 0 $arg1 ogl_diffg
-		chkey -t 0 -F 'ch("i_colorg")' $arg1/ogl_diffg
-		chadd -t 0 0 $arg1 ogl_diffb
-		chkey -t 0 -F 'ch("i_colorb")' $arg1/ogl_diffb
+			chadd -t 0 0 $arg1 ogl_diffr
+			chkey -t 0 -F 'ch("i_colorr")' $arg1/ogl_diffr
+			chadd -t 0 0 $arg1 ogl_diffg
+			chkey -t 0 -F 'ch("i_colorg")' $arg1/ogl_diffg
+			chadd -t 0 0 $arg1 ogl_diffb
+			chkey -t 0 -F 'ch("i_colorb")' $arg1/ogl_diffb
+		endif
 
 	endif
 
@@ -27,10 +27,10 @@ if ( "$arg1" != "" ) then
 		set ogl_diff_rough_def = `run("opparm -ql $arg1 ogl_diff_rough")`
 		if("$ogl_diff_rough_def" == "") then
 			opproperty -f -F OpenGL $arg1 material* ogl_diff_rough
-		endif
 
-		chadd -t 0 0 $arg1 ogl_diff_rough
-		chkey -t 0 -F 'ch("roughness")' $arg1/ogl_diff_rough
+			chadd -t 0 0 $arg1 ogl_diff_rough
+			chkey -t 0 -F 'ch("roughness")' $arg1/ogl_diff_rough
+		endif
 
 	endif
 
@@ -40,10 +40,10 @@ if ( "$arg1" != "" ) then
 		set ogl_metallic_def = `run("opparm -ql $arg1 ogl_metallic")`
 		if("$ogl_metallic_def" == "") then
 			opproperty -f -F OpenGL $arg1 material* ogl_metallic
-		endif
 
-		chadd -t 0 0 $arg1 ogl_metallic
-		chkey -t 0 -F 'ch("metallic")' $arg1/ogl_metallic
+			chadd -t 0 0 $arg1 ogl_metallic
+			chkey -t 0 -F 'ch("metallic")' $arg1/ogl_metallic
+		endif
 
 	endif
 
@@ -53,14 +53,14 @@ if ( "$arg1" != "" ) then
 		set ogl_emit_def = `run("opparm -ql $arg1 ogl_emit")`
 		if("$ogl_emit_def" == "") then
 			opproperty -f -F OpenGL $arg1 material* ogl_emit
-		endif
 
-		chadd -t 0 0 $arg1 ogl_emitr
-		chkey -t 0 -F 'ch("incandescencer")' $arg1/ogl_emitr
-		chadd -t 0 0 $arg1 ogl_emitg
-		chkey -t 0 -F 'ch("incandescenceg")' $arg1/ogl_emitg
-		chadd -t 0 0 $arg1 ogl_emitb
-		chkey -t 0 -F 'ch("incandescenceb")' $arg1/ogl_emitb
+			chadd -t 0 0 $arg1 ogl_emitr
+			chkey -t 0 -F 'ch("incandescencer")' $arg1/ogl_emitr
+			chadd -t 0 0 $arg1 ogl_emitg
+			chkey -t 0 -F 'ch("incandescenceg")' $arg1/ogl_emitg
+			chadd -t 0 0 $arg1 ogl_emitb
+			chkey -t 0 -F 'ch("incandescenceb")' $arg1/ogl_emitb
+		endif
 
 	endif
 
@@ -70,15 +70,15 @@ if ( "$arg1" != "" ) then
 		set ogl_emit_intensity_def = `run("opparm -ql $arg1 ogl_emit_intensity")`
 		if("$ogl_emit_intensity_def" == "") then
 			opproperty -f -F OpenGL $arg1 material* ogl_emit_intensity
-		endif
 
-		chadd -t 0 0 $arg1 ogl_emit_intensity
-		chkey -t 0 -F 'ch("incandescence_intensity")' $arg1/ogl_emit_intensity
+			chadd -t 0 0 $arg1 ogl_emit_intensity
+			chkey -t 0 -F 'ch("incandescence_intensity")' $arg1/ogl_emit_intensity
+		endif
 
 	endif
 
 	set ogl_spec_model_def = `run("opparm -ql $arg1 ogl_spec_model")`
-	if("$ogl_spec_model_def" == "" && "$i_color_def" != "" && "$roughness_def"=="" ) then
+	if("$ogl_spec_model_def" == "" && "$i_color_def" != "" && "$roughness_def" != "") then
 		opproperty -f -F OpenGL $arg1 material* ogl_spec_model
 		opparm $arg1 ogl_spec_model ggx
 	endif
