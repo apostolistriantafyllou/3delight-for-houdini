f75770f4e3acf8f0c7a1e654e9f4de5aaf8493b9
Aghiles Kheffache <aghiles@3delight.com>
2020-03-06 07:36:14 -0500

    Don't remove old GEO attributes anymore.
    This generates a lot of warnings when uysing Hython. Warnings that we do not
    see when sstarting  Maya but that end up flooding logs and users's console that
    are using Hython. By now, these old attributes shouldn't exist anyway.
    
    Note that we still get warning on new attribute creation, if the attributes
    already exist. We will have to get rid of these too.

--- ui/geo_OnCreated.cmd
+++ ui/geo_OnCreated.cmd
@@ -3,50 +3,6 @@
 
 \set noalias = 1
 if ( "$arg1" != "" ) then
-# Removed old names (without prefix) and badly named
-# space override names
-
-   opspare -d obj_geo_separator1 $arg1
-   opspare -d obj_geo_label1 $arg1
-   opspare -d obj_geo_separator2 $arg1
-   opspare -d space2 $arg1
-   opspare -d vis_diffuse $arg1
-   opspare -d compositing $arg1
-   opspare -d obj_geo_separator3 $arg1
-   opspare -d obj_geo_label2 $arg1
-   opspare -d obj_geo_separator4 $arg1
-   opspare -d space3 $arg1
-   opspare -d deformation $arg1
-   opspare -d add_samples $arg1
-   opspare -d obj_geo_separator5 $arg1
-   opspare -d obj_geo_label3 $arg1
-   opspare -d obj_geo_separator6 $arg1
-   opspare -d override_vol $arg1
-   opspare -d over_compositing_enable $arg1
-   opspare -d over_compositing $arg1
-   opspare -d over_vis_camera_enable $arg1
-   opspare -d over_vis_camera $arg1
-   opspare -d over_vis_diffuse_enable $arg1
-   opspare -d over_vis_diffuse $arg1
-   opspare -d over_vis_reflection_enable $arg1
-   opspare -d over_vis_reflection $arg1
-   opspare -d over_vis_refraction_enable $arg1
-   opspare -d over_vis_refraction $arg1
-   opspare -d override_ss $arg1
-   opspare -d _3dl_override_vol $arg1
-   opspare -d _3dl_over_compositing_enable $arg1
-   opspare -d _3dl_over_compositing $arg1
-   opspare -d _3dl_over_vis_camera_enable $arg1
-   opspare -d _3dl_over_vis_camera $arg1
-   opspare -d _3dl_over_vis_diffuse_enable $arg1
-   opspare -d _3dl_over_vis_diffuse $arg1
-   opspare -d _3dl_over_vis_reflection_enable $arg1
-   opspare -d _3dl_over_vis_reflection $arg1
-   opspare -d _3dl_over_vis_refraction_enable $arg1
-   opspare -d _3dl_over_vis_refraction $arg1
-   opspare -d _3dl_override_ss $arg1
-   opspare -d _3dl_vis_diffuse $arg1
-
 # Add properties into tab 3Delight
    opproperty -f $arg1 3Delight _3dl_render_poly_as_subd
    opproperty -f $arg1 3Delight _3dl_smooth_curves
