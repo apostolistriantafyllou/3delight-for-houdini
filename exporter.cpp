apostolistriantafyllou/3delight-for-houdini

commit 3cd6af225ef0631238712b86a8aad6336927522b

Improve scene::convert_to_nsi for nice VOP export
We used to parse scene recursively, looking for objects and vops together,
sometimes getting into nodes we don't want to see. It works, but its a bit
of a mess.

Starting from now, we proceed like this:

1. Scan OBJ nodes (/obj/*)
2. From these objects, gather geometries and get their materials.
3. For each material, export all VOPs, including inputs.

This makes a much cleaner output and find materials that where not even found
before (because we tried to avoid getting into SOPs, and these could contains
VOPs in "matnets"). Now we don't care where the VOPs are hidden as we have the
actual VOP network heads from which we can start our scan.

scene.cpp
@@ -68,52 +68,23 @@ static bool object_displayed(
 	and that is renderable in the current frame range and obey to Display
 	flag / Objects to Render.
 */
-void scene::process_node(
+void scene::process_obj_node(
 	const context &i_context,
-	OP_Node *i_node,
+	OBJ_Node *obj,
 	bool i_check_visibility,
 	std::vector<exporter *> &o_to_export,
 	std::deque<safe_interest>& io_interests )
 {
-	if( i_node->castToVOPNode() )
-	{
-		/*
-			The material builder is just a container, we don't need to generate
-			any node for it.
-		*/
-		if( i_node->getOperator()->getName() == "dlMaterialBuilder" )
-			return;
-
-		/*
-		   Our material network connections is done :) The connection
-		   between nodes is done in \ref vop::connect
-		   Read vop.cpp if you are bewildered by this simplicity.
-		*/
-		o_to_export.push_back(
-			new vop(i_context, i_node->castToVOPNode()) );
-		if(i_context.m_ipr)
-		{
-			io_interests.emplace_back(
-				i_node,
-				const_cast<context*>(&i_context),
-				&vop::changed_cb);
-		}
-		return;
-	}
-
-	OBJ_Node *obj = i_node->castToOBJNode();
-	if( !obj )
-		return;
-
 	/*
 		Each object is its own null transform. Note that this null transform
 		could be removed down below if object export doesn't occur.
 	*/
 	o_to_export.push_back( new null(i_context, obj) );
+
 	if(i_context.m_ipr)
 	{
 		io_interests.emplace_back(
-			i_node,
+			obj,
 			const_cast<context*>(&i_context),
 			&null::changed_cb);
 	}
@@ -137,7 +108,7 @@ void scene::process_node(
 			if(i_context.m_ipr)
 			{
 				io_interests.emplace_back(
-					i_node,
+					obj,
 					const_cast<context*>(&i_context),
 					&light::changed_cb);
 			}
@@ -156,7 +127,7 @@ void scene::process_node(
 		if(i_context.m_ipr)
 		{
 			io_interests.emplace_back(
-				i_node,
+				obj,
 				const_cast<context*>(&i_context),
 				&camera::changed_cb);
 		}
@@ -187,12 +158,78 @@ void scene::process_node(
 }
 
 /**
-	\brief Scans for geometry, cameras, lights, vops etc... and produces
-	a list of exporters.
-
-	\ref exporter
+	\brief Go through all used materials and produce VOPs exporters.
 */
-void scene::scan(
+void scene::vop_scan(
+	const context &i_context,
+	std::vector<exporter *> &io_to_export,
+	std::deque<safe_interest>& io_interests )
+{
+	std::unordered_set< std::string > materials;
+	for( auto E : io_to_export )
+	{
+		geometry *geo = dynamic_cast<geometry*>( E );
+		if( !geo )
+			continue;
+		geo->get_all_material_paths( materials );
+	}
+
+	std::unordered_set<VOP_Node *> vops;
+	for( const auto &M : materials )
+	{
+		VOP_Node *mat = OPgetDirector()->findVOPNode( M.data() );
+
+		if( !mat )
+			continue;
+
+		vops.insert( mat );
+	}
+
+	std::vector<VOP_Node*> traversal;
+	for( auto V : vops )
+		traversal.push_back( V );
+
+	/* re-purpose this set for de-duplication of VOPs */
+	vops.clear();
+
+	while( traversal.size() )
+	{
+		VOP_Node* node = traversal.back();
+		traversal.pop_back();
+
+		if( !node )
+			continue;
+
+		if( vops.find(node) == vops.end() )
+		{
+			io_to_export.push_back( new vop(i_context, node) );
+			if(i_context.m_ipr)
+			{
+				io_interests.emplace_back(
+					node, const_cast<context*>(&i_context), &vop::changed_cb);
+			}
+			vops.insert( node );
+		}
+
+		int ninputs = node->nInputs();
+		for( int i = 0; i < ninputs; i++ )
+		{
+			VOP_Node *input = CAST_VOPNODE( node->getInput(i) );
+			if( !input )
+				continue;
+			traversal.push_back( input );
+		}
+	}
+
+	//printf( "Exported %lu materials\n", vops.size() );
+	return;
+}
+
+/**
+	\brief Scans for geometry, cameras, lights and produces a list of exporters.
+	\ref geometry
+*/
+void scene::obj_scan(
 	const context &i_context,
 	std::vector<exporter *> &o_to_export,
 	std::deque<safe_interest>& io_interests )
@@ -200,48 +237,25 @@ void scene::scan(
 	/* A traversal stack to avoid recursion */
 	std::vector< OP_Node * > traversal;
 
-	OP_Node *our_dear_leader = OPgetDirector();
-	traversal.push_back( our_dear_leader->findNode( "/obj") );
-	traversal.push_back( our_dear_leader->findNode( "/mat") );
+	OP_Node *root = OPgetDirector()->findNode( "/obj" );
 
 	/*
 		After this while loop, to_export will be filled with the OBJs
 		and the VOPs to convert to NSI.
 	*/
-	while( traversal.size() )
+	int nkids = root->getNchildren();
+	for( int i=0; i< nkids; i++ )
 	{
-		OP_Node *network = traversal.back();
-		traversal.pop_back();
+		OP_Node *node = root->getChild(i);
+		OBJ_Node *obj = node->castToOBJNode();
 
-		assert( network->isNetwork() );
-
-		int nkids = network->getNchildren();
-		for( int i=0; i< nkids; i++ )
+		if( !obj )
 		{
-			OP_Node *node = network->getChild(i);
-
-			/* Don't get into SOP networks. Nothing to see there */
-			if( node->castToSOPNode() )
-				continue;
-
-			if( node->castToVOPNode() &&
-				!vop::is_renderable(node->castToVOPNode()) )
-			{
-				/* Don't go inide VOPs we can't render */
-				continue;
-			}
-
-			process_node( i_context, node, true, o_to_export, io_interests );
-
-			if( !node->isNetwork() )
-				continue;
-
-			OP_Network *kidnet = (OP_Network *)node;
-			if( kidnet->getNchildren() )
-			{
-				traversal.push_back( kidnet );
-			}
+			assert( false );
+			continue;
 		}
+
+		process_obj_node( i_context, obj, true, o_to_export, io_interests );
 	}
 }
 
@@ -292,12 +306,12 @@ void scene::scan_for_instanced(
 	*/
 	for( auto E : instanced )
 	{
-		OP_Node *o = OPgetDirector()->findNode( E.data() );
+		OBJ_Node *o = OPgetDirector()->findOBJNode( E.data() );
 		if( !o )
 			continue;
 
 		int s = io_to_export.size(); assert( s>0 );
-		process_node( i_context, o, false, io_to_export, io_interests );
+		process_obj_node( i_context, o, false, io_to_export, io_interests );
 
 		/*
 		   Finally, make sure we don't render the source geometry as its
@@ -315,21 +329,29 @@ void scene::scan_for_instanced(
 	\brief Contains the high-level logic of scene conversion to
 	a NSI representation.
 
-	\see process_node
+	\see process_obj_node
 */
 void scene::convert_to_nsi(
 	const context &i_context,
 	std::deque<safe_interest>& io_interests )
 {
-	/* Start be getting the list of exporter for the scene */
+	/*
+		Start be getting the list of all OBJ exporters.
+	*/
 	std::vector<exporter *> to_export;
-	scan( i_context, to_export, io_interests );
+	obj_scan( i_context, to_export, io_interests );
 
 	/*
 		Make sure instanced geometry is included in the list, regardless of
 		display flag or scene elements.
 	*/
 	scan_for_instanced( i_context, to_export, io_interests );
+
+	/*
+		Now, for the OBJs that are geometries, gather the list of materials and
+		build a list of VOP exporters for these.
+	*/
+	vop_scan( i_context, to_export, io_interests );
 
 	/*
 		Create phase. This will create all the main NSI nodes from the Houdini

geometry.cpp
@@ -800,3 +800,19 @@ VOP_Node *geometry::get_assigned_material( std::string &o_path ) const
 	return resolve_material_path( material_path.c_str(), o_path );
 }
 
+/**
+	Return all the materials needed by this geometry.
+*/
+void geometry::get_all_material_paths(
+	std::unordered_set< std::string > &o_materials ) const
+{
+	for( auto P : m_primitives )
+	{
+		P->get_all_material_paths( o_materials );
+	}
+
+	std::string obj_mat;
+	get_assigned_material( obj_mat );
+
+	o_materials.insert( obj_mat );
+}

geometry.h
@@ -3,6 +3,8 @@
 #include "exporter.h"
 
 #include <vector>
+#include <string>
+#include <unordered_set>
 
 class primitive;
 class instance;
@@ -34,6 +36,10 @@ public:
 
 	/** \ref scene::scan_for_instanced */
 	void get_instances( std::vector<const instance *> & ) const;
+
+	/** \brief Return all the materials needed by this geometry. */
+	void get_all_material_paths(
+		std::unordered_set< std::string > &o_materials ) const;
 
 private:
 	/**

primitive.cpp
@@ -340,4 +340,23 @@ void primitive::get_bind_attributes(
 	}
 }
 
+void primitive::get_all_material_paths(
+	std::unordered_set< std::string > &o_materials ) const
+{
+	GT_Owner owner;
+	GT_DataArrayHandle materials = default_gt_primitive().get()->findAttribute(
+		"shop_materialpath", owner, 0);
 
+	if( !materials )
+		return;
+
+	for( int i=0; i<materials->entries() ; i++ )
+	{
+		const char *path = materials->getS( i );
+		std::string resolved;
+		exporter::resolve_material_path( path, resolved );
+
+		if( !resolved.empty() )
+			o_materials.insert( resolved );
+	}
+}

primitive.h
@@ -3,6 +3,8 @@
 #include "exporter.h"
 
 #include <vector>
+#include <unordered_set>
+#include <string>
 
 /// Base class for exporters of refined GT primitives.
 class primitive : public exporter
@@ -93,6 +95,11 @@ protected:
 	*/
 	void export_bind_attributes( OP_Node *i_obj_level_material ) const;
 
+	/**
+		Return all the materials needed by this geometry.
+	*/
+	void get_all_material_paths(
+		std::unordered_set< std::string > &o_materials ) const;
 private:
 	/**
 		\brief Returns attributes that are needed by the given materials.

scene.h
@@ -41,7 +41,12 @@ public:
 	static void find_custom_aovs( std::vector<VOP_Node*>& o_custom_aovs );
 
 private:
-	static void scan(
+	static void obj_scan(
+		const context &i_context,
+		std::vector< exporter * > &o_to_export,
+		std::deque<safe_interest>& io_interests );
+
+	static void vop_scan(
 		const context &i_context,
 		std::vector< exporter * > &o_to_export,
 		std::deque<safe_interest>& io_interests );
@@ -51,9 +56,9 @@ private:
 		std::vector<exporter *> &io_to_export,
 		std::deque<safe_interest>& io_interests );
 
-	static void process_node(
+	static void process_obj_node(
 		const context &i_context,
-		OP_Node *i_node,
+		OBJ_Node *,
 		bool i_check_visibility,
 		std::vector<exporter *> &o_to_export,
 		std::deque<safe_interest>& io_interests );

commit 0552b10e1c327a6c70153b30a32c242165c60759

Fix export of atmopshere.
We do that in scene::vop_scan. Was broken in 3cd6af225ef0631238712.

exporter.cpp
@@ -241,7 +241,7 @@ std::string exporter::absolute_path(
 }
 
 VOP_Node *exporter::resolve_material_path(
-	const char *i_path,  std::string &o_path ) const
+	OP_Node *i_node, const char *i_path,  std::string &o_path )
 {
 	OP_Node* op_node = OPgetDirector()->findNode( i_path );
 	VOP_Node *vop_node = nullptr;
@@ -254,7 +254,7 @@ VOP_Node *exporter::resolve_material_path(
 	if( !vop_node )
 	{
 		/* Try a relative search */
-		vop_node = m_object->findVOPNode( i_path );
+		vop_node = i_node->findVOPNode( i_path );
 		if( !vop_node )
 		{
 			return nullptr;
@@ -264,6 +264,7 @@ VOP_Node *exporter::resolve_material_path(
 	o_path = vop_node->getFullPath().toStdString();
 	return vop_node;
 }
+
 /**
 */
 NSI::Context&

exporter.h
@@ -76,6 +76,14 @@ public:
 	/** utility to resolve relative paths */
 	static std::string absolute_path( OP_Node *i_node, const char *i_path );
 
+	/**
+		\brief Resolves the [relaative] material path to an absolute
+		path and a VOP node.
+	*/
+	static VOP_Node *resolve_material_path(
+		OP_Node *i_needed_relative_paths,
+		const char *i_path,  std::string &o_path );
+
 protected:
 
 	/**
@@ -105,14 +113,11 @@ protected:
 		double i_time,
 		GT_DataArrayHandle i_vertices_list = GT_DataArrayHandle()) const;
 
-protected:
-
-	/**
-		\brief Resolves the [relaative] material path to an absolute
-		path and a VOP node.
-	*/
 	VOP_Node *resolve_material_path(
-		const char *i_path,  std::string &o_path ) const;
+		const char *i_path,  std::string &o_path ) const
+	{
+		return resolve_material_path(m_object, i_path, o_path );
+	}
 
 	/**
 		\brief Returns the context where to export (possibly static) attributes.

scene.cpp
@@ -175,6 +175,9 @@ void scene::process_obj_node(
 
 /**
 	\brief Go through all used materials and produce VOPs exporters.
+
+	We also check the Atmopshere shader in the ROP, as we can't possibliy find
+	it otherwise.
 */
 void scene::vop_scan(
 	const context &i_context,
@@ -237,7 +240,31 @@ void scene::vop_scan(
 		}
 	}
 
-	//printf( "Exported %lu materials\n", vops.size() );
+	/* Deal with atmosphere */
+	OP_Node *rop = OPgetDirector()->findNode( i_context.m_rop_path.c_str() );
+	if( !rop )
+	{
+		return;
+	}
+
+	int index;
+	if( (index=rop->getParmIndex("atmosphere")) == -1 )
+		return;
+
+	ROP_3Delight *r3 = (ROP_3Delight *) CAST_ROPNODE( rop );
+	UT_String atmosphere_path;
+	rop->evalString( atmosphere_path, "atmosphere", 0, r3->current_time() );
+
+	if( atmosphere_path.length() > 0 )
+	{
+		std::string resolved;
+		VOP_Node *atmosphere_shader = exporter::resolve_material_path(
+			r3, atmosphere_path.c_str(), resolved );
+
+		if( atmosphere_shader )
+			io_to_export.push_back( new vop(i_context, atmosphere_shader) );
+	}
+
 	return;
 }
 