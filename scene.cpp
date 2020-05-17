apostolistriantafyllou/3delight-for-houdini

commit 26332b856f850e91714936ea82c607af7f95c548

Fix visiblity bug introduced with OBJ instancer.
The visiblity became hierarchical: if the parent was not visible the children
would be invisible too. This hapenned because I decided to "clean" the null
transforms of invisible nodes. This is was required for the instancer
implementation.

Note that the good visiblity behaviour was working by "mistake" as was not
something we paid attention to. At least know we know why we need to output
a null for each object, even if invisble. :)

scene.cpp
@@ -77,7 +77,8 @@ void scene::process_obj_node(
 {
 	/*
 		Each object is its own null transform. Note that this null transform
-		could be removed down below if object export doesn't occur.
+		is *needed* if object is invisible because it could potentially have
+		visible children.
 	*/
 	o_to_export.push_back( new null(i_context, obj) );
 
@@ -141,12 +142,6 @@ void scene::process_obj_node(
 			rop_path,
 			i_context.m_current_time))
 	{
-		/*
-			Delete the useless null exporter. This is important
-			as our instanced geometry detection relies on invsible
-			objects handles not being output.
-		*/
-		delete o_to_export.back(); o_to_export.pop_back();
 		return;
 	}
 
@@ -303,7 +298,23 @@ void scene::scan_for_instanced(
 		if( it == instanced.end() )
 			continue;
 
-		instanced.erase( it );
+		OP_Node *node = E->node();
+		OBJ_Node *obj = CAST_OBJNODE( node );
+		if( !obj )
+		{
+			/* shouldn't be part of instanced! */
+			assert( false );
+			continue;
+		}
+
+		if( object_displayed(
+				*obj,
+				i_context.m_lights_to_render_pattern,
+				i_context.m_rop_path.data(),
+				i_context.m_current_time))
+		{
+			instanced.erase( it );
+		}
 	}
 
 	/*
@@ -556,7 +567,13 @@ void scene::export_light_categories(
 {
 	assert( i_exporter );
 
-	OBJ_Node *i_object = i_exporter->obj();
+	OP_Node *node = i_exporter->node();
+
+	OBJ_Node *i_object = CAST_OBJNODE( node );
+	if( !i_object )
+	{
+		return;
+	}
 
 	UT_String categories;
 	int lightcategories_index = i_object->getParmIndex("lightcategories");

exporter.h
@@ -5,6 +5,7 @@
 /* WARNING: The order of the following two includes is important  { */
 #include <GT/GT_Primitive.h>
 #include <GT/GT_Handles.h>
+#include <OBJ/OBJ_Node.h>
 /* }  */
 
 #include <nsi.h>
@@ -13,7 +14,6 @@
 #include <vector>
 
 class context;
-class OBJ_Node;
 class VOP_Node;
 namespace NSI { class Context; }
 
@@ -59,7 +59,7 @@ public:
 	*/
 	const std::string &handle( void ) const;
 
-	OBJ_Node *obj( void ) { return m_object; }
+	OP_Node *node( void ) { return m_object; }
 
 	/**
 		\brief Declare this as an instanced object. Such objects

commit 223967b3e0a0ea42850627c24609d0807685e8f7

Fix instanced object visibility.
It was broken in 26332b856f850e91714936ea82. Invisible objects that are
instanced still appeared in the render because there were two null transforms
output for said object.

scene.cpp
@@ -32,8 +32,13 @@
 /**
 	\brief Decide what to do with the given OP_Node.
 
-	\param i_check_visibility
-		= true if must check Display flag / Objects to Render.
+	\param i_re_export_instanced.
+		= true if this run is for re exporting and instanced object that was
+		ignored beause it was invisible. We find out about such objects later
+		in the pipeline. When re-exportig, we don't check Display flag /
+		Objects to Render (as this would return 'false') and we don't need
+		to exprt the NULL transform at the top because it has alredy been
+		exported in the first run. \ref scan_for_instanced
 
 	Houdini's OBJ nodes will correspond to NSI transform. So we insert
 	a null exported for each one of these.
@@ -45,15 +50,19 @@
 void scene::process_obj_node(
 	const context &i_context,
 	OBJ_Node *obj,
-	bool i_check_visibility,
+	bool i_re_export_instanced,
 	std::vector<exporter *> &o_to_export )
 {
 	/*
-		Each object is its own null transform. Note that this null transform
-		is *needed* if object is invisible because it could potentially have
-		visible children.
+		Each object is its own null transform. When re-exporting an invisible
+		object that was tgged as an instance, we don't need to output the
+		null exported as it is already present since the first scene
+		scan.
 	*/
-	o_to_export.push_back( new null(i_context, obj) );
+	if( !i_re_export_instanced )
+	{
+		o_to_export.push_back( new null(i_context, obj) );
+	}
 
 	if(i_context.m_ipr)
 	{
@@ -65,9 +74,11 @@ void scene::process_obj_node(
 		return;
 	}
 
+	bool check_visibility = !i_re_export_instanced;
+
 	if( obj->castToOBJLight() )
 	{
-		if(!i_check_visibility || i_context.object_displayed(*obj) )
+		if(!check_visibility || i_context.object_displayed(*obj) )
 		{
 			o_to_export.push_back( new light(i_context, obj) );
 		}
@@ -94,7 +105,7 @@ void scene::process_obj_node(
 		return;
 	}
 
-	bool visible = !i_check_visibility || i_context.object_displayed(*obj);
+	bool visible = !check_visibility || i_context.object_displayed(*obj);
 
 	/*
 		Check for special node IncandescenceLight.
@@ -338,7 +349,7 @@ void scene::obj_scan(
 
 			if( obj )
 			{
-				process_obj_node( i_context, obj, true, o_to_export );
+				process_obj_node( i_context, obj, false, o_to_export );
 			}
 
 			traversal.push_back( node );
@@ -408,14 +419,14 @@ void scene::scan_for_instanced(
 		if( !o )
 			continue;
 
-		int s = io_to_export.size(); assert( s>0 );
-		process_obj_node( i_context, o, false, io_to_export );
+		process_obj_node(
+			i_context, o, true /* re-export instance */, io_to_export );
 
 		/*
 		   Finally, make sure we don't render the source geometry as its
 		   only rendered through instancing.
 		*/
-		for( int i=s-1; i<io_to_export.size(); i++ )
+		for( int i=0; i<io_to_export.size(); i++ )
 		{
 			if( io_to_export[i]->handle() == E )
 				io_to_export[i]->set_as_instanced();

scene.h
@@ -81,7 +81,7 @@ private:
 	static void process_obj_node(
 		const context &i_context,
 		OBJ_Node *,
-		bool i_check_visibility,
+		bool i_re_export_instanced,
 		std::vector<exporter *> &o_to_export );
 
 	/**