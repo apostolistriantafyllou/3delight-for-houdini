apostolistriantafyllou/3delight-for-houdini

commit d2b01ff266818fd19df4c7e63405e9a950178fe9

Removed the 3Delight Material Builder from the export logic in "scene" class.
There is no exporter for the Material Builder, and we actually don't need one
since it has no equivalence in NSI.  This is one less thing to worry about.

This avoids exporting the builder's "main" material and its connections twice
(once with the builder's handle, once with the actual shader's handle). This
caused changes to the material to go unnoticed in IPR because, while the
"material" shader was updated in the NSI scene, NSI objects were actually
connected to the "builder" shader.
It also eliminates the VOP's "Am i just a material builder? And do i even refer
to a valid object?" identity crisis introduced in
2a1a43a911b7607b8261509f22a4d295a661f565 . In particular, it eliminates the
comparison of the operator's name with incorrect string "dlMaterialBuilder",
which should have been updated in 3120b97c4abf72fcf071194e66f87ffbfc3c0376 .

primitive.cpp
@@ -394,11 +394,11 @@ void primitive::assign_sop_materials( void ) const
 	{
 		/*
 			Could be a detail attribute or just one-faced poly, no need
-			to go further as we can just create one attirbute node.
+			to go further as we can just create one attribute node.
 		*/
-		const std::string shop( materials->getS(0) );
+		std::string shop( materials->getS(0) );
 
-		if( !shop.empty() )
+		if( !resolve_material_path(m_object, shop.c_str(), shop) )
 		{
 			std::string attribute_handle = m_handle + shop;
 
@@ -422,10 +422,11 @@ void primitive::assign_sop_materials( void ) const
 	std::unordered_map< std::string, std::vector<int> > all_materials;
 	for( int i=0; i<materials->entries(); i++ )
 	{
-		const std::string shop( materials->getS(i) );
-
-		if( !shop.empty() )
+		std::string shop( materials->getS(i) );
+		if( resolve_material_path(m_object, shop.c_str(), shop) )
+		{
 			all_materials[ shop ].push_back( i );
+		}
 	}
 
 	/* Create the NSI face sets + attributes and connect to geo */

exporter.cpp
@@ -1,6 +1,8 @@
 #include "exporter.h"
 
 #include "context.h"
+#include "vop.h"
+#include "VOP_3DelightMaterialBuilder.h"
 
 #include <nsi.hpp>
 
@@ -261,11 +263,23 @@ VOP_Node *exporter::resolve_material_path(
 	{
 		/* Try a relative search */
 		vop_node = i_node->findVOPNode( i_path );
-		if( !vop_node )
-		{
-			o_path.clear();
-			return nullptr;
-		}
+	}
+
+	/*
+		If the material is actually a 3Delight MaterialBuilder, use its "main
+		material" instead so the builders is kept out of the export logic.
+	*/
+	VOP_3DelightMaterialBuilder* builder =
+		dynamic_cast<VOP_3DelightMaterialBuilder*>(vop_node);
+	if(builder)
+	{
+		vop_node = vop::get_builder_material(builder);
+	}
+
+	if(!vop_node)
+	{
+		o_path.clear();
+		return nullptr;
 	}
 
 	o_path = vop_node->getFullPath().toStdString();

instance.cpp
@@ -304,6 +304,8 @@ void instance::get_merge_points(
 		std::string M =	(material && i<material->entries()) ?
 					(const char*)(material->getS(i)) : "";
 
+		resolve_material_path(m_object, M.c_str(), M);
+
 		std::pair< std::string, std::string > m = std::make_pair(I, M);
 
 		if( o_uniqued.find(m) == o_uniqued.end() )

scene.cpp
@@ -214,17 +214,6 @@ void scene::create_materials_exporters(
 				node, const_cast<context*>(&i_context), &vop::changed_cb);
 		}
 
-		/*
-			If this was a material builder, also recurse inside the assigned
-			material itself.
-		*/
-		std::string op = node->getOperator()->getName().toStdString();
-		if( op == "3Delight::dlMaterialBuilder" )
-		{
-			traversal.push_back( vop::get_builder_material( node ) );
-			continue;
-		}
-
 		int ninputs = node->nInputs();
 		for( int i = 0; i < ninputs; i++ )
 		{

vop.cpp
@@ -19,19 +19,10 @@ vop::vop(
 :
 	exporter( i_ctx, i_vop )
 {
-	std::string op = i_vop->getOperator()->getName().toStdString();
-
-	if( op == "3Delight::dlMaterialBuilder" )
-	{
-		m_vop = get_builder_material( i_vop );
-	}
 }
 
 void vop::create( void ) const
 {
-	if( !m_object )
-		return;
-
 	const shader_library &library = shader_library::get_instance();
 	std::string path = library.get_shader_path( vop_name().c_str() );
 	if( path.length() == 0 )
@@ -44,17 +35,11 @@ void vop::create( void ) const
 
 void vop::set_attributes( void ) const
 {
-	if( !m_object )
-		return;
-
 	set_attributes_at_time(m_context.m_current_time);
 }
 
 void vop::set_attributes_at_time( double i_time ) const
 {
-	if( !m_object )
-		return;
-
 	NSI::ArgumentList list;
 	std::string uv_coord_connection;
 
@@ -688,10 +673,6 @@ bool vop::is_renderable( VOP_Node *i_vop )
 {
 	const shader_library &library = shader_library::get_instance();
 	std::string vop_name( i_vop->getOperator()->getName().toStdString() );
-
-	if( vop_name == "dlMaterialBuilder" )
-		return true;
-
 	std::string path = library.get_shader_path( vop_name.c_str() );
 
 	return !path.empty();

commit e57a1cfa1549c82a9022b96553156b1da679193a

Fixed inverted condition.
This bug was introduced in d2b01ff266818fd19df4c7e63405e9a950178fe9 .

primitive.cpp
@@ -400,7 +400,8 @@ void primitive::assign_sop_materials( void ) const
 		*/
 		std::string shop( materials->getS(0) );
 
-		if( !resolve_material_path(m_object, shop.c_str(), shop) )
+
+		if( resolve_material_path(m_object, shop.c_str(), shop) )
 		{
 			std::string attribute_handle = m_handle + shop;
 