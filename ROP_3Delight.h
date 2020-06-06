01e15dc95b3fa2f9942d5a290a5a88559ab744d3
Aghiles Kheffache <aghiles@3delight.com>
2020-06-06 18:52:29 -0400

    Untangle some declarations

--- ROP_3Delight.h
+++ ROP_3Delight.h
@@ -181,15 +181,27 @@ private:
 		std::map<std::string, std::vector<OBJ_Node*>> &, fpreal t) const;
 
 	bool HasSpeedBoost( double i_time )const;
+
 	/// Retrieves the image resolution, scaled by the Speed Boost res factor
 	bool GetScaledResolution(int& o_x, int& o_y)const;
+
+	/** Returns the resolution multiplier from Speed Boost */
 	float GetResolutionFactor()const;
+
+	/** Returns the sampling multiplier from Speed Boost */
 	float GetSamplingFactor()const;
+
+	/** Returns the pixel samples (AA oversampling) in this ROP */
 	int GetPixelSamples()const;
+
+	/** Returns camera used to Render. */
 	OBJ_Camera* GetCamera( double i_time )const;
+
 	double GetShutterInterval(double i_time)const;
+
 	bool HasDepthOfField( double i_time )const;
 
+private:
 	std::vector<OBJ_Node*> m_lights;
 	bool m_cloud;
 
