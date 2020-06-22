6c214e412acdfa4164f52c138cc7086bf250702e
Francois Colbert <fc@3delight.com>
2020-06-22 18:52:44 -0400

    Warned the user about mismatched build and run-time Houdini versions.
    Backward (and forward) compatibility doesn't exist with the HDK.

--- plugin.cpp
+++ plugin.cpp
@@ -4,8 +4,33 @@
 #include "creation_callbacks.h"
 #include "shader_library.h"
 
+#include <HOM/HOM_Module.h>
+#include <HOM/HOM_ui.h>
 #include <UT/UT_DSOVersion.h>
+#include <UT/UT_Version.h>
 
+static void check_houdini_version()
+{
+	const char build_version[] =
+		SYS_VERSION_MAJOR "." SYS_VERSION_MINOR "." SYS_VERSION_BUILD;
+	std::string runtime_version = ::HOM().applicationVersionString();
+
+	if(runtime_version != build_version)
+	{
+		std::cerr
+			<< "3Delight for Houdini: This 3Delight package is configured to be "
+				"used with Houdini " << build_version << ".\n"
+			<< "Using it with another version of Houdini (currently, "
+			<< runtime_version << ") might result in crashes and other problems."
+			<< std::endl;
+	}
+}
+
+extern "C" SYS_VISIBILITY_EXPORT void
+HoudiniDSOInit(UT_DSOInfo&)
+{
+	check_houdini_version();
+}
 
 extern "C" SYS_VISIBILITY_EXPORT void
 newDriverOperator(OP_OperatorTable* io_table)
