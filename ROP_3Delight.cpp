apostolistriantafyllou/3delight-for-houdini

commit 87286c7d122f7dd25cbcd83a8460ed14e73e7386

Fix custom AOV scanning.
We now take into consideration objects visibility when scanning for
AOVs. Also, we don't scan just any VOP in the scene, only these
that are somehow connected to rendered materials.

There is some code re-organisation so that I can reuse VOP scanning
and object visiility logic. I decided to make a new class for the
later as I feel we might need this again.

ROP_3Delight.cpp
@@ -6,6 +6,7 @@
 #include "exporter.h"
 #include "idisplay_port.h"
 #include "object_attributes.h"
+#include "object_visibility_resolver.h"
 #include "scene.h"
 #include "shader_library.h"
 #include "vdb.h"
@@ -176,9 +177,6 @@ ROP_3Delight::~ROP_3Delight()
 void ROP_3Delight::onCreated()
 {
 	ROP_Node::onCreated();
-	std::vector<VOP_Node*> custom_aovs;
-	scene::find_custom_aovs(custom_aovs);
-	aov::updateCustomVariables(custom_aovs);
 }
 
 void ROP_3Delight::StartRenderFromIDisplay(
@@ -436,6 +434,7 @@ int ROP_3Delight::startRender(int, fpreal tstart, fpreal tend)
 			settings::k_rm_live_render;
 
 	m_current_render = new context(
+		m_settings,
 		m_nsi,
 		m_static_nsi,
 		tstart,
@@ -447,11 +446,7 @@ int ROP_3Delight::startRender(int, fpreal tstart, fpreal tend)
 		ipr,
 		!render,
 		m_cloud,
-		getFullPath().toStdString(),
-		m_settings.OverrideDisplayFlags(),
-		m_settings.GetObjectsToRender(),
-		m_settings.GetLightsToRender(),
-		m_settings.get_matte_objects() );
+		getFullPath().toStdString() );
 
 	m_end_time = tend;
 

context.cpp
@@ -1,27 +1,11 @@
 #include "context.h"
 
-#include "OBJ/OBJ_Node.h"
-
-bool
-context::object_displayed(const OBJ_Node& i_node)const
+bool context::object_displayed( const OBJ_Node& i_node ) const
 {
-	OP_BundlePattern* pattern =
-		const_cast<OBJ_Node&>(i_node).castToOBJLight()
-		?	m_lights_to_render_pattern
-		:	m_objects_to_render_pattern;
-
-	if(pattern)
-	{
-		return pattern->match(&i_node, m_rop_path.c_str(), true);
-	}
-
-	return i_node.getObjectDisplay(m_current_time);
+	return m_object_visibility_resolver.object_displayed( i_node );
 }
 
-bool
-context::object_is_matte(const OBJ_Node& i_node)const
+bool context::object_is_matte( const OBJ_Node& i_node ) const
 {
-	return
-		m_mattes_pattern &&
-		m_mattes_pattern->match(&i_node, m_rop_path.c_str(), true);
+	return m_object_visibility_resolver.object_is_matte( i_node );
 }

context.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #include "safe_interest.h"
+#include "object_visibility_resolver.h"
 
 #include <nsi.hpp>
 
-#include <OP/OP_BundlePattern.h>
 #include <SYS/SYS_Types.h>
 #include <UT/UT_TempFileManager.h>
 
@@ -14,6 +14,7 @@
 #include <vector>
 
 class OBJ_Node;
+class ROP_3Delight;
 
 /**
 	\brief An export context passed around to each exporter. Allows us
@@ -25,6 +26,7 @@ class context
 {
 public:
 	context(
+		const settings &i_settings,
 		NSI::Context &i_nsi,
 		NSI::Context &i_static_nsi,
 		fpreal i_start_time,
@@ -36,11 +38,7 @@ public:
 		bool i_ipr,
 		bool i_export_nsi,
 		bool i_cloud,
-		const std::string& i_rop_path,
-		bool i_override_display_flags,
-		const UT_String& i_objects_to_render,
-		const UT_String& i_lights_to_render,
-		const UT_String& i_matte_objects )
+		const std::string& i_rop_path)
 	:
 		m_nsi(i_nsi),
 		m_static_nsi(i_static_nsi),
@@ -55,29 +53,13 @@ public:
 		m_export_nsi(i_export_nsi),
 		m_cloud(i_cloud),
 		m_rop_path(i_rop_path),
-		m_objects_to_render_pattern(
-			i_override_display_flags
-			? OP_BundlePattern::allocPattern(i_objects_to_render)
-			: nullptr),
-		m_lights_to_render_pattern(
-			i_override_display_flags
-			? OP_BundlePattern::allocPattern(i_lights_to_render)
-			: nullptr),
-		m_mattes_pattern( OP_BundlePattern::allocPattern(i_matte_objects) )
+		m_object_visibility_resolver(i_rop_path, i_settings, i_start_time)
 	{
 		assert(!m_ipr || !m_export_nsi);
 	}
 
 	~context()
 	{
-		if( m_lights_to_render_pattern &&
-			m_objects_to_render_pattern &&
-			m_mattes_pattern )
-		{
-			OP_BundlePattern::freePattern(m_lights_to_render_pattern);
-			OP_BundlePattern::freePattern(m_objects_to_render_pattern);
-			OP_BundlePattern::freePattern(m_mattes_pattern);
-		}
 
 		for( const auto &f : m_temp_filenames )
 		{
@@ -148,16 +130,10 @@ public:
 	// Full path of the 3Delight ROP from where rendering originates
 	std::string m_rop_path;
 
-	// Objects export filter
-	OP_BundlePattern* m_objects_to_render_pattern;
-	// Lights export filter
-	OP_BundlePattern* m_lights_to_render_pattern;
-
-	/** All these objects will be tagged as matte */
-	OP_BundlePattern* m_mattes_pattern;
-
 	/** files to be deleted at render end. */
 	mutable std::vector< std::string > m_temp_filenames;
+
+	object_visibility_resolver m_object_visibility_resolver;
 
 private:
 	/*

scene.cpp
@@ -178,25 +178,22 @@ void scene::vop_scan(
 	}
 
 	create_materials_exporters( materials, i_context, io_to_export );
-	
+
 	/* Deal with atmosphere */
 	create_atmosphere_shader_exporter( i_context, io_to_export );
 }
 
 /**
-	\brief Creates the exporters for all shaders node required by each material.
-	
+	\brief Returns all the vops traversed by a set of materials
+
 	\param i_materials
-		List of materials paths for which to create exporters.
-	\param i_context
-		Current rendering context.
-	\param io_to_export
-		New exporters will be appended here.
+		List of materials paths to explore for VOPs
+	\param o_vops
+		VOPs poduced during scan
 */
-void scene::create_materials_exporters(
+void scene::get_material_vops(
 	const std::unordered_set<std::string>& i_materials,
-	const context &i_context,
-	std::vector<exporter *> &io_to_export )
+	std::vector<VOP_Node*> &o_vops )
 {
 	std::unordered_set<VOP_Node *> vops;
 	for( const auto &M : i_materials )
@@ -229,11 +226,7 @@ void scene::create_materials_exporters(
 
 		vops.insert( node );
 
-		io_to_export.push_back( new vop(i_context, node) );
-		if(i_context.m_ipr)
-		{
-			i_context.register_interest(node, &vop::changed_cb);
-		}
+		o_vops.push_back( node );
 
 		int ninputs = node->nInputs();
 		for( int i = 0; i < ninputs; i++ )
@@ -243,6 +236,35 @@ void scene::create_materials_exporters(
 				continue;
 			traversal.push_back( input );
 		}
+	}
+}
+
+/**
+	\brief Creates the exporters for all shaders node required by each material.
+
+	\param i_materials
+		List of materials paths for which to create exporters.
+	\param i_context
+		Current rendering context.
+	\param io_to_export
+		New exporters will be appended here.
+*/
+void scene::create_materials_exporters(
+	const std::unordered_set<std::string>& i_materials,
+	const context &i_context,
+	std::vector<exporter *> &io_to_export )
+{
+	std::vector<VOP_Node *> vops;
+	get_material_vops( i_materials, vops );
+
+	for( auto &V : vops )
+	{
+		if(i_context.m_ipr)
+		{
+			i_context.register_interest(V, &vop::changed_cb);
+		}
+
+		io_to_export.push_back( new vop(i_context,V) );
 	}
 }
 
@@ -326,7 +348,7 @@ void scene::obj_scan(
 
 /**
 	\brief Export instanced objects that might have been skipped because
-	of visiblity.
+	of visibility.
 */
 void scene::scan_for_instanced(
 	const context &i_context,
@@ -578,14 +600,17 @@ void scene::find_lights(
 	}
 }
 
-void scene::find_custom_aovs( std::vector<VOP_Node*>& o_custom_aovs )
+void scene::find_custom_aovs(
+	const object_visibility_resolver &i_resolver,
+	std::vector<VOP_Node*>& o_custom_aovs )
 {
 	/* A traversal stack to avoid recursion */
 	std::vector< OP_Node * > traversal;
 
 	OP_Node *our_dear_leader = OPgetDirector();
-	traversal.push_back( our_dear_leader->findNode( "/mat") );
 	traversal.push_back( our_dear_leader->findNode( "/obj") );
+
+	std::unordered_set< std::string > materials;
 
 	while( traversal.size() )
 	{
@@ -598,10 +623,19 @@ void scene::find_custom_aovs( std::vector<VOP_Node*>& o_custom_aovs )
 		for( int i=0; i< nkids; i++ )
 		{
 			OP_Node *node = network->getChild(i);
-			VOP_Node *vop_node = node->castToVOPNode();
-			if( vop_node && vop::is_aov_definition(vop_node) )
+			OBJ_Node *obj_node = node->castToOBJNode();
+
+			if( !obj_node || !i_resolver.object_displayed(*obj_node) )
 			{
-				o_custom_aovs.push_back(vop_node);
+				continue;
+			}
+
+			OP_Node* material =
+				obj_node->getMaterialNode(i_resolver.m_current_time);
+
+			if( material )
+			{
+				materials.insert( material->getFullPath().toStdString() );
 			}
 
 			if( !node->isNetwork() )
@@ -613,6 +647,15 @@ void scene::find_custom_aovs( std::vector<VOP_Node*>& o_custom_aovs )
 				traversal.push_back( kidnet );
 			}
 		}
+	}
+
+	std::vector< VOP_Node * > vops;
+	get_material_vops( materials, vops );
+
+	for( auto vop : vops )
+	{
+		if( vop::is_aov_definition(vop) )
+			o_custom_aovs.push_back( vop );
 	}
 }
 
@@ -676,7 +719,7 @@ void scene::export_light_categories(
 		if(io_lights_to_render.empty())
 		{
 			find_lights(
-				i_context.m_lights_to_render_pattern,
+				i_context.m_object_visibility_resolver.m_lights_to_render_pattern,
 				i_context.m_rop_path.c_str(),
 				false,
 				io_lights_to_render);

CMakeLists.txt
@@ -53,6 +53,7 @@ add_library( ${library_name} SHARED
 	primitive.cpp
 	null.cpp
 	object_attributes.cpp
+	object_visibility_resolver.cpp
 	osl_utilities.cpp
 	plugin.cpp
 	safe_interest.cpp

object_visibility_resolver.cpp
@@ -1,0 +1,61 @@
+#include <OP/OP_BundlePattern.h>
+#include <OBJ/OBJ_Node.h>
+
+#include "object_visibility_resolver.h"
+#include "ui/settings.h"
+
+object_visibility_resolver::object_visibility_resolver(
+	const std::string &i_rop_path, const settings &i_settings,
+	double i_time )
+:
+	m_current_time(i_time)
+{
+	UT_String lights_to_render, objects_to_render;
+	bool override_display_flags = i_settings.OverrideDisplayFlags();
+
+	if( override_display_flags )
+	{
+		m_objects_to_render_pattern =
+			OP_BundlePattern::allocPattern( i_settings.GetObjectsToRender() );
+
+		m_lights_to_render_pattern =
+			OP_BundlePattern::allocPattern( i_settings.GetLightsToRender() );
+	}
+
+	m_mattes_pattern =
+		OP_BundlePattern::allocPattern( i_settings.get_matte_objects() );
+}
+
+object_visibility_resolver::~object_visibility_resolver()
+{
+	if( m_lights_to_render_pattern &&
+		m_objects_to_render_pattern )
+	{
+		OP_BundlePattern::freePattern( m_lights_to_render_pattern );
+		OP_BundlePattern::freePattern( m_objects_to_render_pattern );
+	}
+
+	OP_BundlePattern::freePattern( m_mattes_pattern );
+}
+
+bool object_visibility_resolver::object_displayed(const OBJ_Node& i_node)const
+{
+	OP_BundlePattern* pattern =
+		const_cast<OBJ_Node&>(i_node).castToOBJLight()
+		? m_lights_to_render_pattern
+		: m_objects_to_render_pattern;
+
+	if(pattern)
+	{
+		return pattern->match(&i_node, m_rop_path.c_str(), true);
+	}
+
+	return i_node.getObjectDisplay( m_current_time );
+}
+
+bool object_visibility_resolver::object_is_matte(const OBJ_Node& i_node)const
+{
+	return
+		m_mattes_pattern &&
+		m_mattes_pattern->match(&i_node, m_rop_path.c_str(), true);
+}

object_visibility_resolver.h
@@ -1,0 +1,29 @@
+#pragma once
+
+#include <string>
+
+class settings;
+class OP_BundlePattern;
+class OBJ_Node;
+
+/**
+	\brief Encapsulates logic needed to decide if an object/light is
+	visible or not.
+*/
+struct object_visibility_resolver
+{
+	object_visibility_resolver(
+		const std::string &i_rop, const settings &, double i_time );
+	~object_visibility_resolver( );
+
+	bool object_displayed( const OBJ_Node& ) const;
+	bool object_is_matte( const OBJ_Node& ) const;
+
+	OP_BundlePattern* m_objects_to_render_pattern{nullptr};
+	OP_BundlePattern* m_lights_to_render_pattern{nullptr};
+	OP_BundlePattern* m_mattes_pattern{nullptr};
+
+	std::string m_rop_path;
+
+	double m_current_time;
+};

scene.h
@@ -9,6 +9,7 @@ class SOP_Node;
 class UT_String;
 class VOP_Node;
 class OP_BundlePattern;
+struct object_visibility_resolver;
 
 namespace NSI { class Context; }
 
@@ -44,7 +45,9 @@ public:
 	/**
 		\brief Find the bind export nodes that can produce custom AOVs
 	*/
-	static void find_custom_aovs( std::vector<VOP_Node*>& o_custom_aovs );
+	static void find_custom_aovs(
+		const object_visibility_resolver &,
+		std::vector<VOP_Node*>& o_custom_aovs );
 
 private:
 	static void obj_scan(
@@ -54,6 +57,11 @@ private:
 	static void vop_scan(
 		const context &i_context,
 		std::vector< exporter * > &o_to_export );
+
+	static void get_material_vops(
+		const std::unordered_set<std::string>& i_materials,
+		std::vector<VOP_Node*> &o_vops );
+
 	static void create_materials_exporters(
 		const std::unordered_set<std::string>& i_materials,
 		const context &i_context,

commit e6522f8504a7248ce6a1bfb93dbd7149c64ce1ce

Fixed animated visibility of objects in frame sequence rendering.
This had been broken in 87286c7d122f7dd25cbcd83a8460ed14e73e7386 by freezing
the time used as getObjectDisplay's parameter into the
object_visibility_resolver.

New method context::set_current_time() ensures that m_current_time and
m_object_visibility_resolver stay synchronized. m_current_time was made const to
avoid it being changed directly without going through the set_current_time()
method. It seemed too late to make it private, though, since it's already used
in so many places, but an accessor was added as an attempt to stop the bleeding.

ROP_3Delight.cpp
@@ -545,7 +545,7 @@ ROP_3Delight::renderFrame(fpreal time, UT_Interrupt*)
 	assert(m_current_render);
 	assert(m_nsi.Handle() == NSI_BAD_CONTEXT);
 
-	m_current_render->m_current_time = time;
+	m_current_render->set_current_time(time);
 
 	std::string frame_nsi_file;
 	if(m_current_render->m_export_nsi)

context.cpp
@@ -11,20 +11,40 @@ context::context(
 	m_start_time(i_time),
 	m_end_time(i_time), m_current_time(i_time),
 	m_rop_path(i_rop->getFullPath().toStdString()),
-	m_object_visibility_resolver(
-		i_rop->getFullPath().toStdString(), i_rop->get_settings(), i_time),
 	m_nsi(s_bad_context),
-	m_static_nsi(s_bad_context)
+	m_static_nsi(s_bad_context),
+	m_settings(i_rop->get_settings())
 {
+	m_object_visibility_resolver =
+		new object_visibility_resolver(m_rop_path, m_settings, i_time);
 }
-
 
 bool context::object_displayed( const OBJ_Node& i_node ) const
 {
-	return m_object_visibility_resolver.object_displayed( i_node );
+	return m_object_visibility_resolver->object_displayed( i_node );
 }
 
 bool context::object_is_matte( const OBJ_Node& i_node ) const
 {
-	return m_object_visibility_resolver.object_is_matte( i_node );
+	return m_object_visibility_resolver->object_is_matte( i_node );
+}
+
+const OP_BundlePattern* context::lights_to_render()const
+{
+	assert(m_object_visibility_resolver);
+	return m_object_visibility_resolver->m_lights_to_render_pattern;
+}
+
+void context::set_current_time(fpreal i_time)
+{
+	/*
+		m_current_time was made const to ensure that it's not modified directly
+		from outside the class, while still keeping it public because it was
+		already widely used. But here is the right place to modify it.
+	*/
+	const_cast<fpreal&>(m_current_time) = i_time;
+
+	delete m_object_visibility_resolver;
+	m_object_visibility_resolver =
+		new object_visibility_resolver(m_rop_path, m_settings, i_time);
 }

context.h
@@ -64,18 +64,21 @@ public:
 		m_archive(i_archive),
 		m_cloud(i_cloud),
 		m_rop_path(i_rop_path),
-		m_object_visibility_resolver(i_rop_path, i_settings, i_start_time)
+		m_settings(i_settings)
 	{
 		assert(!m_ipr || !m_export_nsi);
+		m_object_visibility_resolver =
+			new object_visibility_resolver(i_rop_path, i_settings, i_start_time);
 	}
 
 	~context()
 	{
-
 		for( const auto &f : m_temp_filenames )
 		{
 			UT_TempFileManager::removeTempFile( f.data() );
 		}
+
+		delete m_object_visibility_resolver;
 	}
 
 	/// Returns true if motion blur is required for this render
@@ -110,6 +113,9 @@ public:
 	/// Returns true if an object is in the mattes bundle.
 	bool object_is_matte(const OBJ_Node& i_node)const;
 
+	/// Returns the bundle pattern built from the "lights to render" setting
+	const OP_BundlePattern* lights_to_render()const;
+	
 	/**
 		\brief Registers a callback to be notified of changes to a node.
 
@@ -124,11 +130,22 @@ public:
 		m_interests.emplace_back(i_node, const_cast<context*>(this), i_cb);
 	}
 
+	/**
+		\brief Returns the animation time used for rendering.
+		
+		Please use this instead of directly accessing the m_current_time member.
+	*/
+	fpreal current_time()const { return m_current_time; }
+	
+	/// Sets the animation time to be used for rendering.
+	void set_current_time(fpreal i_time);
+
 public:
 	NSI::Context &m_nsi;
 	NSI::Context &m_static_nsi;
 	fpreal m_start_time{0.0f}, m_end_time{.0f};
-	fpreal m_current_time{.0f};
+	/// Animation time to render (please use current_time() accessor instead).
+	const fpreal m_current_time{.0f};
 	fpreal m_frame_duration{.0f};
 	fpreal m_shutter{.0f};
 	// True if depth-of-field is enabled
@@ -145,9 +162,12 @@ public:
 	/** files to be deleted at render end. */
 	mutable std::vector< std::string > m_temp_filenames;
 
-	object_visibility_resolver m_object_visibility_resolver;
-
 private:
+
+	object_visibility_resolver* m_object_visibility_resolver;
+
+	const settings& m_settings;
+
 	/*
 		List of interests (callbacks) created in IPR mode.
 		We don't use a vector because there might be a lot of items (1 or 2 per

scene.cpp
@@ -774,7 +774,7 @@ void scene::export_light_categories(
 		if(io_lights_to_render.empty())
 		{
 			find_lights(
-				i_context.m_object_visibility_resolver.m_lights_to_render_pattern,
+				i_context.lights_to_render(),
 				i_context.m_rop_path.c_str(),
 				false,
 				io_lights_to_render);