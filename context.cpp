900505be91eb22520fa7d42e206cf6bce5c0b6ba
ogers.ruda <ogers.ruda@gmail.com>
2021-01-05 16:45:38 +0100

    Updated Matte Objects in ROP to only require objects of that list. Fixes #314
    If we were to override the Objects to Render list, and have several
    objects on Matte Objects list, then we should have added these objects
    on "objects to render list" too, in order to render them as mattes.
    Now objects that are added to Matte Objects are automatically rendered
    without needing to add them on Objects to Render list.

--- context.cpp
+++ context.cpp
@@ -87,7 +87,8 @@ context::~context()
 
 bool context::object_displayed( const OBJ_Node& i_node ) const
 {
-	return m_object_visibility_resolver->object_displayed( i_node );
+	return m_object_visibility_resolver->object_displayed( i_node )
+		|| m_object_visibility_resolver->object_is_matte( i_node );
 }
 
 bool context::object_is_matte( const OBJ_Node& i_node ) const
