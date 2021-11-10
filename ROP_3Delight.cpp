apostolistriantafyllou/3delight-for-houdini

commit 5c897410a244994e82efd61dad3fbcf27ae454df

Used proper delimiters to quote a string.
This seemed to be the original intent. With single-quotes, the compiler
interprets it as a multi-byte character. It complained on Linux, at least, but
the absence of a warning on other platforms didn't mean that it worked...

ROP_3Delight.cpp
@@ -1707,9 +1707,9 @@ ROP_3Delight::BuildImageUniqueName(
 	/*
 		Checking for the last '_' character to find the frame number is not a good
 		idea and results in wrong behavior when another character is used before the
-		frame keyword '$F'.
+		frame keyword "$F".
 	*/
-	size_t pos = fn.find_last_of('$F');
+	size_t pos = fn.find_last_of("$F");
 	UT_String frame_number = fn.assign(fn.begin()+pos-1, fn.end()).c_str();
 
 	OPgetDirector()->getChannelManager()->expandString(

commit 06b14b8b63ee147a95957a5292a7db9627ebf387

Used std::string::rfind() instead of std::string::find_last_of().
This fixes wrong code introduced in 5c897410a244994e82efd61dad3fbcf27ae454df :
rfind() searches for a substring, while find_last_of() searches for any
character in a list.

ROP_3Delight.cpp
@@ -1706,7 +1706,7 @@ ROP_3Delight::BuildImageUniqueName(
 		idea and results in wrong behavior when another character is used before the
 		frame keyword "$F".
 	*/
-	size_t pos = fn.find_last_of("$F");
+	size_t pos = fn.rfind("$F");
 	UT_String frame_number = fn.assign(fn.begin()+pos-1, fn.end()).c_str();
 
 	OPgetDirector()->getChannelManager()->expandString(