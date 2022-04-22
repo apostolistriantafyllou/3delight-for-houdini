a294543bece056ae9dc0ec3e98fef149d0e8c6fc
Francois Colbert <fc@3delight.com>
2022-04-22 11:40:08 -0400

    Added support for Cryptomatte AOVs in Deep EXR output.
    Otherwise, the IDs just get output to a regular one-channel layer which, apart
    from hitting a small bug in the 3Delight library (the crash described in issue
    https://gitlab.com/3Delight/3delight-core/-/issues/157), is of no use to Nuke's
    Cryptomatte plugin.
    
    So, here we are : however strange that might seem, we now output a
    depth-simulating Cryptomatte ID into a non-deep layer of an otherwise deep EXR
    file.

--- ROP_3Delight.cpp
+++ ROP_3Delight.cpp
@@ -1554,7 +1554,8 @@ ROP_3Delight::ExportOneOutputLayer(
 	unsigned cryptomatte_layers = 0;
 	if(i_desc.m_variable_name.substr(0, 3) == "id." &&
 		i_desc.m_variable_source == "builtin" &&
-		(i_driver_name == "exr" || i_driver_name == "dwaaexr"))
+		(i_driver_name == "exr" || i_driver_name == "dwaaexr" ||
+		i_driver_name == "deepexr"))
 	{
 		cryptomatte_layers = 2;
 	}
